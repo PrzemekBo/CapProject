8d55a4e49653e88730f94812f7f3ce247b20ad0f
PBOCER <przemek.bocer@gmail.com>
2018-07-12 11:00:38 +0200

    Initialize

--- src/messages/StandardMessages.java
+++
@@ -1,8 +0,0 @@
-package messages;
-
-/**
- * Created by Marcin on 25.02.2017.
- */
-public final class StandardMessages {
-    public static final String METHOD_NOT_IMPLEMENTED = "Method is not implemented!";
-}
